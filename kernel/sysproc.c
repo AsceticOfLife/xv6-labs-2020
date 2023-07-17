7810ca96285e3f593a18b3e7118ea2f8980d1c98
Guomw <isguo_mw@163.com>
2023-07-17 18:14:20 +0800

    complete lab-lazy

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -47,8 +47,13 @@ sys_sbrk(void)
   if(argint(0, &n) < 0)
     return -1;
   addr = myproc()->sz;
-  if(growproc(n) < 0)
-    return -1;
+
+  if (n < 0) 
+    uvmdealloc(myproc()->pagetable, addr, addr + n);
+    
+  myproc()->sz = myproc()->sz + n;
+  // if(growproc(n) < 0)
+  //   return -1;
   return addr;
 }
 
